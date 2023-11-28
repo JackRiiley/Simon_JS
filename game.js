0498100f920c87d27e04dc554b24cd68a290a4bd
Jack Riley <jaackriiley@gmail.com>
2023-11-28 23:15:47 +0000

    Check user's answer against Game
    Created a new function 'checkAnswer', this is called when a user clicks
    a button. The index of the last answer is passed to the function.
    This user answer is then checked against the gamePattern. If it is
    correct 'nextSequence' is called after a delay and the user pattern is
    reset.

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ let userClickedPattern = [];
 let level = 0;
 let started = false;
 
+// Detects when a key has been pressed to start the game
 $(document).keypress(function () {
     if (!started) {
         nextSequence();
@@ -13,13 +14,31 @@ $(document).keypress(function () {
     $("#level-title").text("Level " + level);
 })
 
+// Detects when a button has been clicked in the browser, this is stored
+// in an array to check whether it matches the gamePattern
 $(".btn").click(function () {
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 })
 
+// Handles checking the userPattern to the gamePattern
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[userClickedPattern.length - 1] === gamePattern[gamePattern.length - 1]) {
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+            userClickedPattern = [];
+        }
+    } else {
+        
+    }
+}
+
+// Handles a simple animation for key presses
 function animatePress(currentColour) {
     $("#" + currentColour).addClass("pressed");
     setTimeout(function () {
@@ -27,6 +46,8 @@ function animatePress(currentColour) {
     }, 100);
 }
 
+// Handles the audio elements so that this can be used in both the user
+// clicks and within the nextSequence fuunction
 function playSound(name) {
     let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
